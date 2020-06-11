6b319405f20e85c23183c7cd10211b12071f183f
priteeshh <priteeshh@gmail.com>
2020-06-12 00:47:08 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import React from 'react';
 function App() {
   return (
     <div>
-      Phone Directory
+      {/* <div className="header">
+        Phone Directory
+    </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span><br></br>
+        <span>Phone</span>
+      </div> */}
+      <label htmlFor="name">Name</label>
+      <input id="name" defaultValue="Preeteeesg"></input>
     </div>
   );
 }
